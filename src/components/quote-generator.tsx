43b566fdc5f8cff2be1c443d03a5c31054fbde67
Taing Bunsou <taing.bunsou24@kit.edu.kh>
2025-09-07 08:14:34 +0700

    refactor: remove hardcoded quotes from QuoteGenerator component

--- src/components/quote-generator.tsx
+++ src/components/quote-generator.tsx
@@ -4,16 +4,6 @@ import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import axios from "axios";
 
-const quotes = [
-  "If the will is powerful, the how is easy. - Jim Rohn",
-  "The only way to do great work is to love what you do. - Steve Jobs",
-  "Life is what happens to you while you're busy making other plans. - John Lennon",
-  "The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
-  "It is during our darkest moments that we must focus to see the light. - Aristotle",
-  "Success is not final, failure is not fatal: it is the courage to continue that counts. - Winston Churchill",
-  "The only impossible journey is the one you never begin. - Tony Robbins",
-];
-
 export function QuoteGenerator() {
   const [currentQuote, setCurrentQuote] = useState<
     string | { quote: string; author: string }
